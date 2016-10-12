431d16bf61e9fd9f3dc593618ef4cf9258559e00
Johan Kämpe <kampe.johan@gmail.com>
2016-10-12 19:16:07 +0200

    Funktion för omspel, fler färgkodet, uppdaterat default-spel

--- text_dyn.c
+++ text_dyn.c
@@ -14,15 +14,10 @@
 Spelmotor för textäventyr med extern textfil
 
 Version 2016-10-10
-Fixat: 
 Initial release
 Spelet fungerar utifrån textfilen.
 Spelet kan avslutas utifrån textfilens val
-
-Version 2016-10-10v2
 Check för inmatning av val, med do-sats
-
-Version 2016-10-10v3
 Debug-switch
 
 Version 2016-10-11
@@ -34,29 +29,30 @@ Det går att starta spelet med en textfil som argument, då laddas denna fil ex: '
 Namngivning av program: SPELMOTOR TXT
 Borttagning av onödiga deklarerade variabler som inte användes.
 
+Version 2016-10-12
+Stöd för titel av spel, skrivs på översta raden i textfilen
+Stöd för omspel J/N
+Fler sifferkoder tillagda för färgbyte vid avslut av spel
 
 Att göra:
 Kommentera koden
 Fler koder för TXXX|YYYY| Där Y kan vara färgbyte, pip, omstart-fråga etc 9998 9997 lämpligen...
 Spellogo, ex L0000| ASCII ART
-
-
 PROBLEM INMATNING AV ICKESIFFRA
+TITEL MANUAL!!!!!!
 
 */
 int listaVal(int a, FILE *f);
 void skrivUtText(char *string, int n, _Bool linjer);
-void textSwitchar(int s);
+_Bool textSwitchar(int s);
 
 //sätt till 1 för att se diverse info från funktionen
 _Bool debugMode = 0;
 
 int main(int argc, char *argv[]){
-	
 	FILE * textfil = fopen(argv[1], "r");
-	char s[N], filnamn[N];
-	int siffra = 1000, idCheck, switchCheck;
-	
+	char s[N], filnamn[N], restart = 'j';
+	int siffra, idCheck, switchCheck;
 	if(textfil == NULL){
 		//Programmet har inte fått en textfil som argument, använder standard.
 		textfil = fopen(TEXTFIL, "r");
@@ -72,33 +68,41 @@ int main(int argc, char *argv[]){
 			exit(0);
 		}
 	}
-	printf("%sVälkommen till SPELMOTOR TXT!\nSpel som kommer köras är: %s %s", L, L, L); //FIXA TITEL I TEXTFIL!!!
-	system("pause");
-
-	
-	while(TTS(s, N, textfil)){
+	while(tolower(restart) == 'j'){
+		siffra = 1000;
+		TTS(s, N, textfil); //Läser in textfilens första rad (som ska vara spelets titel) till variabel 's'
+		printf("%sVälkommen till SPELMOTOR TXT!\nSpel som kommer köras är: %s %s", L, s, L);
+		system("pause");
 		system("cls");
-		sscanf(s+1, "%d", &idCheck);
-		if(s[0] == 'T' && idCheck == siffra){
-			skrivUtText(s+LT, strlen(s)-LT, 1);
-			sscanf(s+6,"%d", &switchCheck);
-			textSwitchar(switchCheck); //FIXA SÅ SWITCH RETURNERAR 1/0 om spelet ska avslutas, lägg switch innan textutskrift
-			siffra = listaVal(siffra, textfil);
-			rewind(textfil);
-		} 
+		while(TTS(s, N, textfil)){
+			sscanf(s+1, "%d", &idCheck);
+			if(s[0] == 'T' && idCheck == siffra){
+				skrivUtText(s+LT, strlen(s)-LT, 1);
+				sscanf(s+6,"%d", &switchCheck);
+				if(!textSwitchar(switchCheck)){
+					skrivUtText("Spelet är Slut|Tack!", 20, 1);
+					break;
+				}
+				siffra = listaVal(siffra, textfil);
+				rewind(textfil);
+			} 
+		}
+	rewind(textfil);
+	clearBuffer();
+	printf("Tryck J för att spela igen: ");
+	restart = getchar();
+	system("color 7"); //Återställer textfärgen till vit
 	}
 	return 0;
 }
 
 int listaVal(int a, FILE *f){
-	int valraknare = 1, check, val, nasta;
+	int valraknare = 1, check, val = 0, nasta;
 	_Bool korrektVal = 1;
 	char s[N];
-	//TTS 'TextfilTillString' finns i strings_text_v1.c
 	while(TTS(s, N, f)){
 		sscanf(s+1, "%d", &check);
 		if(s[0] == 'V' && check == a+valraknare){
-			//printf("[%d] %s\n", valraknare, s+5+6);
 			printf("[%d] ", valraknare);
 			skrivUtText(s+LT, strlen(s)-LT, 0);
 			printf("\n");
@@ -107,17 +111,19 @@ int listaVal(int a, FILE *f){
 	}
 	valraknare--;
 	if(debugMode){printf("\nFunktion räknare: %d", valraknare);}
+	//Do-sats som upprepas om användaren inte matar in ett korrekt val.
 	do{
 		if(korrektVal){
 			printf("%sAnge val genom at slå in en siffra: ",L);
 		}
 		else {
-			printf("%sAnge val genom at slå in en KORREKT siffra: ",L);
+			printf("%sFel val!\nAnge val genom at slå in en siffra: ",L);
 		}
 		scanf("%d", &val);
 		if(debugMode){printf("\nval Input do-sats: %d, räknare: %d", val, valraknare);}
 		korrektVal = 0;
 	} while(val <=0 || val > valraknare);
+
 	rewind(f);
 		while(TTS(s, N, f)){
 		sscanf(s+1, "%d", &check);
@@ -140,26 +146,37 @@ void skrivUtText(char *string, int n, _Bool linjer){
 		else{
 			putchar(string[i]);
 		}
-		//TEXTHASTIGHET definieras som macro i kodens början
 		Sleep(TEXTHASTIGHET);
 	}
 	if(linjer){ 
 		printf("%s", L);
 	}
 }
-void textSwitchar(int s){ //RETURNERA 1/0 för att fortsätta spela etc
+
+//Funktionen returnerar 0 om spelet ska avslutas, annars 1
+_Bool textSwitchar(int s){
 	if(debugMode){printf("Switch-funktion: Nummer: %d", s);}
+	//break; behövs inte för switch-satsen inte funktionen avslutas när returvärde skickas.
 	switch(s){
 		case 9999:
-			//printf("%sSpelet är slut! - Tack!%s",L,L);
-			skrivUtText("Spelet är Slut|Tack!", 20, 1); //FIXA J/N
-			exit(0);
-			break;
-		case 9998:
-			//för test    ---- temporär
+			//Spelet avslutas
+			return 0; 
+		case 9996:
+			//Spelet avslutas och texten byter färg till blå
+			system("color 09");
+			return 0;
+		case 9997:
+			//Spelet avslutas och texten byter färg till grön
 			system("color 02");
-			break;
+			return 0;
+		case 9998:
+			//Spelet avslutas och texten byter färg till rött
+			system("color 04");
+			return 0;
+		case 0000:
+			return 1;
 		default:
+			return 1;
 			break;
 	}
 }
