d9a90e3072a5a7c781b35ce2a4641ab640c44137
Johan Kämpe <kampe.johan@gmail.com>
2016-10-13 15:29:01 +0200

    Changed and added comments

--- strings_text_v1.c
+++ strings_text_v1.c
@@ -1,69 +1,110 @@
 #include "strings_text_v1.h"
 
-/*TESTVERSION 1
+/*
+VERSION 1
+
+Ändringar:
+---------------------------------------------------------
 2016-10-05
-behöver uppsnyggning
+Första utgåvan
+---------------------------------------------------------
+2016-10-13
+Uppdaterat kommentarer, inga ändringar i koden
+---------------------------------------------------------
 
-Funktioner för inläsning av text och från textfil till textsträng
+Information:
+Filen innehåller funktioner för inläsning av text, 
+samt kopiering från textfil till char-variabel.
 
-Som författarens i boken, men jag ville bygga dem själv för
-att fatta vad som händer med dem, jag valde också andra namn
+Funktionerna är lika eller identiska till de stödfunktioner som finns
+i boken 'C från början' av Jan Skansholm.
 
-*/
+Anledningen till att ej använda författarens bibliotek som finns
+tillgängligt på bokens hemsida, är att jag ville skriva funktionerna 
+själv för att få en större förståelse för hur de fungerar och 
+varför de bör användas.
 
+Jag använder andra namn på funktionerna, än de namn
+författaren har valt.
+
+Innan varje funktion finns en förklaring för vad den gör
+
+
+
+removeNewLine
+Kontrollerar om det sista tecknet i en textsträng är ett nyradstecken.
+Om så är fallet byts det ut till ett nolltecken och funtkionen ger returvärdet 1.
+Annars ges returvärdet 0, vilket innebär att det kan finnas tecken kvar i bufferten.
+
+Bokens version kallas remove_nl och finns på sida 199 */
 _Bool removeNewLine(char a[]){
-	int i = strlen(a) -1; //näst sista bokstaven i strängen
+	int i = strlen(a) -1;
 	if (a[i] == '\n'){
 		a[i] = '\0';
 		return 1;
 	}
-	//printf("\nTest: removeNewLine returnerar 0, det kan finnas text i buffer!\n");
 	return 0;
 }
 
-/* En annan funktion författaren använder (Sida 190) 
-Om man skriver in en längre text än x i fgets(string, x, stdin) så läggs
-resterande i en buffert som läggs in i nästa inläsning, om man använder denna funktion
-så rensas bufferten */
+/*  
+clearBuffer
+Funktionen rensar den textbuffert som i bland finns kvar vid textinmatning, 
+så att texten inte hamnar i nästa inmatning. Systemet samlar in textinmatning
+en användare gör i bufferten, som inte läses in förrän returslag ges.
+
+Buffert kan finnas vid till exempel fgets(s, n, stdin) om användaren skriver
+fler tecken än n.
+
+Bokens version kallas skip_line och finns på sida 190 */
 void clearBuffer(void){
 	int i;
 	while ((i = getchar()) != '\n' && i != EOF)
 		;
 }
 
-//Liknande funktionen clearBuffer, fast för läsning från textfil
+/*
+clearBufferFil
+Liknande funktionen clearBuffer, fast för läsning från textfil.
+Som parameter får funktionen den aktuella textfilen.
+
+Bokens version kallas fskip_line och finns på sida 226 */
 void clearBufferFil(FILE *f){
 	int i;
 	while ((i = fgetc(f)) != '\n' && i != EOF)
 		;
 }
 
-/* för att slippa använda
-	if (!removeNewLine(sträng))
-		clearBuffer();
-efter varje inläsning med fgets har författaren byggt en egen inläsningsfunktion (Sida 200)
+/*
+radInput
+Funktion som läser in text från användaren till en char-variabel med fgets
+Så länge inläsningen lyckas, så ger funktionen returvärdet 1. Annars 0.
 
-Funktionen ger returvärdet 1 vid en lyckad inläsning, annars 0 (vid EOF)
-den får parametrarna: strängen som ska läsas in, och 'n' för fgets(sträng, n, stdin)
-*/
+removeNewLine anropas, om radInput får tillbaka returvärdet 0 anropas clearBuffer.
+removeNewLine returnerar 0 om den inte "hittar" ett nyradstecken i strängens slut, 
+vilket innebär att det kan finnas text kvar i bufferten, som töms med clearBuffer.
+
+Bokens version kallas read_line och finns på sida 200 */
 _Bool radInput(char a[], int n){
-	//fgets returnerar NULL vid misslyckad inläsning
 	if (fgets(a, n, stdin) == NULL){
 		return 0;	
 	}
-	/*om removeNewLine returnerar 0 (!inverterat) finns det text kvar i buffert, 
-	och clearBuffer-funktionen anropas */
+	/*om removeNewLine returnerar 0 (! = inverterat) finns det text kvar i buffert, 
+	och clearBuffer-funktionen ovan anropas */
 	if (!removeNewLine(a)){ 
 		clearBuffer();	
 	}
 	return 1;
 }
 
-_Bool TTS(char *a, int n, FILE *f){
-	return textfilTillString(a, n, f);
-}
+/*
+textfilTillString
+Lik radInput ovan, dock läses texten från en texfil i stället för stdin
+Som parameter får funktionen variabeln som text ska skrivas till, 
+antal tecken som ska skrivas, samt filen.
 
-//Funktion som läser text från textfil till textsträng, returnerar 0 vid avslut
+clearBufferFil med filen som parameter används i stället för clearBuffer.
+
+Bokens version kallas read_line och finns på sida 200 */
 _Bool textfilTillString(char *a, int n, FILE *f){
 	if (fgets(a, n, f) == NULL){
 		return 0;
@@ -73,3 +114,14 @@ _Bool textfilTillString(char *a, int n, FILE *f){
 	}
 	return 1;
 }
+/*
+TTS
+Anropar textfilTillString och ger dess returvärde som returvärde
+Används som namnförkortning textfilTillString.
+
+Jag lyckades inte ordna det med ett macro eller på annat.
+*/
+_Bool TTS(char *a, int n, FILE *f){
+	return textfilTillString(a, n, f);
+}
+
